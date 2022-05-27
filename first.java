3f898f7ab538072aef56188f9dc2530ae123382b
Larry <raijin22@outlook.com>
2022-05-26 20:54:52 -0800

    first java file

--- first.java
+++ first.java
@@ -1,1 +1,24 @@
-{}
+import java.util.Scanner;
+
+public class first {
+  /*
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter name: ");
+    String firstname = input.next();
+
+    System.out.print("Enter age: ");
+    String age = input.next();
+
+    System.out.println("Your name is " + firstname + " and you are "+ age + "!");
+  }
+  */
+
+  public static void main(String[] args) {
+    System.out.println("I");
+    System.out.println("Like");
+    System.out.println("Ice cream!");
+
+  }
+}
